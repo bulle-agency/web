3bbfb77b52efdb1200eb3e2febf4cafe68dcb40f
jhovadev <jorgearocutipa036@gmail.com>
2025-06-28 22:26:10 -0500

    fix(remove clien:only astro component): remove client:only in astro component
    the cline:only in astro component causes an error in the astro entrypoints build
    
    BREAKING CHANGE: remove client:only in astro component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,46 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
+  vite: {
+      server: {
+          watch: {
+              usePolling: true,
+          },
+      },
 	},
-	site: "https://mintaka.co",
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "es"],
+
+  site: "https://mintaka.co",
+
+  i18n: {
+      defaultLocale: "en",
+      locales: ["en", "es"],
 	},
-	markdown: {
-		drafts: true,
-		shikiConfig: {
-			theme: "css-variables",
-		},
+
+  markdown: {
+      drafts: true,
+      shikiConfig: {
+          theme: "css-variables",
+      },
 	},
-	shikiConfig: {
-		wrap: true,
-		skipInline: false,
-		drafts: true,
+
+  shikiConfig: {
+      wrap: true,
+      skipInline: false,
+      drafts: true,
 	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		mdx(),
-		icon(),
+
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      sitemap(),
+      mdx(),
+      icon(),
 	],
+
+  adapter: vercel(),
 });
